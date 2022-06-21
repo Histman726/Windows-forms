3a04f40a750f8d5403be072bbf85738ccf4d1344
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-06-20 20:14:03 -0500

    Textbox para cadenas

---
+++ Windows Forms/Textbox para cadenas/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Textbox_para_cadenas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Nombre = new System.Windows.Forms.Label();
+            this.txt_Nombre = new System.Windows.Forms.TextBox();
+            this.lbl_Res = new System.Windows.Forms.Label();
+            this.btn_Saludo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_Nombre
+            // 
+            this.lbl_Nombre.AutoSize = true;
+            this.lbl_Nombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Nombre.Location = new System.Drawing.Point(32, 41);
+            this.lbl_Nombre.Name = "lbl_Nombre";
+            this.lbl_Nombre.Size = new System.Drawing.Size(78, 22);
+            this.lbl_Nombre.TabIndex = 0;
+            this.lbl_Nombre.Text = "Nombre:";
+            // 
+            // txt_Nombre
+            // 
+            this.txt_Nombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Nombre.Location = new System.Drawing.Point(116, 38);
+            this.txt_Nombre.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_Nombre.Name = "txt_Nombre";
+            this.txt_Nombre.Size = new System.Drawing.Size(256, 28);
+            this.txt_Nombre.TabIndex = 1;
+            // 
+            // lbl_Res
+            // 
+            this.lbl_Res.AutoSize = true;
+            this.lbl_Res.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Res.Location = new System.Drawing.Point(32, 97);
+            this.lbl_Res.Name = "lbl_Res";
+            this.lbl_Res.Size = new System.Drawing.Size(58, 22);
+            this.lbl_Res.TabIndex = 2;
+            this.lbl_Res.Text = "label1";
+            // 
+            // btn_Saludo
+            // 
+            this.btn_Saludo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Saludo.Location = new System.Drawing.Point(399, 30);
+            this.btn_Saludo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_Saludo.Name = "btn_Saludo";
+            this.btn_Saludo.Size = new System.Drawing.Size(108, 45);
+            this.btn_Saludo.TabIndex = 3;
+            this.btn_Saludo.Text = "Saludar";
+            this.btn_Saludo.UseVisualStyleBackColor = true;
+            this.btn_Saludo.Click += new System.EventHandler(this.btn_Saludo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 149);
+            this.Controls.Add(this.btn_Saludo);
+            this.Controls.Add(this.lbl_Res);
+            this.Controls.Add(this.txt_Nombre);
+            this.Controls.Add(this.lbl_Nombre);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Nombre;
+        private System.Windows.Forms.TextBox txt_Nombre;
+        private System.Windows.Forms.Label lbl_Res;
+        private System.Windows.Forms.Button btn_Saludo;
+    }
+}
+
---
+++ Windows Forms/Textbox para cadenas/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Textbox_para_cadenas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbl_Res.Text = "";
+        }
+
+        private void btn_Saludo_Click(object sender, EventArgs e)
+        {
+            lbl_Res.Text=txt_Nombre.Text+" es el nombre, Hola!!!";
+        }
+    }
+}
