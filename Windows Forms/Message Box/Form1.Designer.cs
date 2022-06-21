b46e946f1fe984a60b4686949087294de98c028f
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-06-21 16:42:36 -0500

    Message Box tipos

---
+++ Windows Forms/Message Box/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Message_Box
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRes = new System.Windows.Forms.Label();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Location = new System.Drawing.Point(74, 53);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(44, 16);
+            this.lblRes.TabIndex = 0;
+            this.lblRes.Text = "label1";
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(199, 53);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 224);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.lblRes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.Button btnMostrar;
+    }
+}
+
---
+++ Windows Forms/Message Box/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Message_Box
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblRes.Text = "";
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            // Cuadros de mensaje
+
+            //? Mensaje
+            //# MessageBox.Show("Hola a todos");
+
+            //? Mensaje con titulo
+            //# MessageBox.Show("Hola a todos", "Mi mensaje");
+
+            //? Mensaje con titulo y botones
+            //# MessageBox.Show("Probando botones", "Pruebas", MessageBoxButtons.AbortRetryIgnore);
+            // Si se necesita saber que boton presiono el usuario se hace de la siguiente manera 
+            /*DialogResult res = MessageBox.Show("Probando botones", "Pruebas", MessageBoxButtons.AbortRetryIgnore);
+                                        if (res == DialogResult.Abort)
+                                            lblRes.Text = "Omitir";
+                                        else if (res == DialogResult.Retry)
+                                            lblRes.Text = "Reintento";
+                                        else if (res == DialogResult.Ignore)
+                                            lblRes.Text = "Ignorar";*/
+
+            //? Mensaje con titulo, botones e icono
+            MessageBox.Show("Probando botones", "Pruebas", MessageBoxButtons.AbortRetryIgnore,MessageBoxIcon.Exclamation);
+        }
+    }
+}
