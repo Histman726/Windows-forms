0f2ee10e8095839408b357b5ad0632d5de749179
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-06-23 21:15:22 -0500

    Menu 1

---
+++ Windows Forms/Menu/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Menu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuSalir = new System.Windows.Forms.ToolStripMenuItem();
+            this.operacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuSumar = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuResta = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuMulti = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuDiv = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuAcerca = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.operacionesToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(396, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuSalir});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // MenuSalir
+            // 
+            this.MenuSalir.Name = "MenuSalir";
+            this.MenuSalir.Size = new System.Drawing.Size(121, 26);
+            this.MenuSalir.Text = "Salir";
+            this.MenuSalir.Click += new System.EventHandler(this.MenuSalir_Click);
+            // 
+            // operacionesToolStripMenuItem
+            // 
+            this.operacionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuSumar,
+            this.MenuResta,
+            this.MenuMulti,
+            this.MenuDiv});
+            this.operacionesToolStripMenuItem.Name = "operacionesToolStripMenuItem";
+            this.operacionesToolStripMenuItem.Size = new System.Drawing.Size(106, 24);
+            this.operacionesToolStripMenuItem.Text = "Operaciones";
+            // 
+            // MenuSumar
+            // 
+            this.MenuSumar.Name = "MenuSumar";
+            this.MenuSumar.Size = new System.Drawing.Size(224, 26);
+            this.MenuSumar.Text = "Suma";
+            this.MenuSumar.Click += new System.EventHandler(this.MenuSumar_Click);
+            // 
+            // MenuResta
+            // 
+            this.MenuResta.Name = "MenuResta";
+            this.MenuResta.Size = new System.Drawing.Size(224, 26);
+            this.MenuResta.Text = "Resta";
+            this.MenuResta.Click += new System.EventHandler(this.MenuResta_Click);
+            // 
+            // MenuMulti
+            // 
+            this.MenuMulti.Name = "MenuMulti";
+            this.MenuMulti.Size = new System.Drawing.Size(224, 26);
+            this.MenuMulti.Text = "Multi";
+            this.MenuMulti.Click += new System.EventHandler(this.MenuMulti_Click);
+            // 
+            // MenuDiv
+            // 
+            this.MenuDiv.Name = "MenuDiv";
+            this.MenuDiv.Size = new System.Drawing.Size(224, 26);
+            this.MenuDiv.Text = "Division";
+            this.MenuDiv.Click += new System.EventHandler(this.MenuDiv_Click);
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuAcerca});
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // MenuAcerca
+            // 
+            this.MenuAcerca.Name = "MenuAcerca";
+            this.MenuAcerca.Size = new System.Drawing.Size(224, 26);
+            this.MenuAcerca.Text = "Acerca de...";
+            this.MenuAcerca.Click += new System.EventHandler(this.MenuAcerca_Click);
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(45, 76);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(178, 22);
+            this.txtA.TabIndex = 1;
+            this.txtA.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(45, 119);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(178, 22);
+            this.txtB.TabIndex = 2;
+            this.txtB.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Location = new System.Drawing.Point(243, 82);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(44, 16);
+            this.lblRes.TabIndex = 3;
+            this.lblRes.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 367);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MenuSalir;
+        private System.Windows.Forms.ToolStripMenuItem operacionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MenuSumar;
+        private System.Windows.Forms.ToolStripMenuItem MenuResta;
+        private System.Windows.Forms.ToolStripMenuItem MenuMulti;
+        private System.Windows.Forms.ToolStripMenuItem MenuDiv;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MenuAcerca;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label lblRes;
+    }
+}
+
---
+++ Windows Forms/Menu/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Menu
+{
+    public partial class Form1 : Form
+    {
+        double a;
+        double b;
+        double r;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblRes.Text = "";
+        }
+
+        private void MenuSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuAcerca_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ejemplo para menu");
+        }
+
+        private void MenuSumar_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(txtA.Text);
+            b = Convert.ToDouble(txtB.Text);
+
+            r = a + b;
+            lblRes.Text = r.ToString();
+        }
+
+        private void MenuResta_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(txtA.Text);
+            b = Convert.ToDouble(txtB.Text);
+
+            r = a - b;
+            lblRes.Text = r.ToString();
+        }
+
+        private void MenuMulti_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(txtA.Text);
+            b = Convert.ToDouble(txtB.Text);
+
+            r = a * b;
+            lblRes.Text = r.ToString();
+        }
+
+        private void MenuDiv_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(txtA.Text);
+            b = Convert.ToDouble(txtB.Text);
+
+            r = a / b;
+            lblRes.Text = r.ToString();
+        }
+    }
+}
