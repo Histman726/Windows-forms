1c974fe48c2a4b2ab6f4e302a315f98682c7c05f
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-06-27 20:04:38 -0500

    Herramienta DomainUpDown

---
+++ Windows Forms/Herramienta DomainUpDown/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Herramienta_DomainUpDown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dupFrutas = new System.Windows.Forms.DomainUpDown();
+            this.txtFrutas = new System.Windows.Forms.TextBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dupFrutas
+            // 
+            this.dupFrutas.Location = new System.Drawing.Point(12, 12);
+            this.dupFrutas.Name = "dupFrutas";
+            this.dupFrutas.Size = new System.Drawing.Size(120, 22);
+            this.dupFrutas.TabIndex = 0;
+            this.dupFrutas.Text = "Frutas";
+            this.dupFrutas.SelectedItemChanged += new System.EventHandler(this.dupFrutas_SelectedItemChanged);
+            // 
+            // txtFrutas
+            // 
+            this.txtFrutas.Location = new System.Drawing.Point(58, 60);
+            this.txtFrutas.Name = "txtFrutas";
+            this.txtFrutas.Size = new System.Drawing.Size(100, 22);
+            this.txtFrutas.TabIndex = 1;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(202, 60);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(91, 23);
+            this.btnAdicionar.TabIndex = 2;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(59, 110);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(44, 16);
+            this.lblInfo.TabIndex = 3;
+            this.lblInfo.Text = "label1";
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(202, 89);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(91, 24);
+            this.btnBorrar.TabIndex = 4;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 388);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.txtFrutas);
+            this.Controls.Add(this.dupFrutas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DomainUpDown dupFrutas;
+        private System.Windows.Forms.TextBox txtFrutas;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Button btnBorrar;
+    }
+}
+
