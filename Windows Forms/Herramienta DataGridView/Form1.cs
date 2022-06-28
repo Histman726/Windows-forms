4f95c98b6ab46de5f384cb95d7128f4534203cf4
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-06-27 19:20:27 -0500

    Herramienta DataGridView

---
+++ Windows Forms/Herramienta DataGridView/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Herramienta_DataGridView
+{
+    public partial class Form1 : Form
+    {
+        private int n=0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            // Adicionamos un nuevo renglon
+            n = dtgvProductos.Rows.Add();
+
+            // Colocamos la informacion
+            dtgvProductos.Rows[n].Cells[0].Value = txtCodigo.Text;
+            dtgvProductos.Rows[n].Cells[1].Value = txtNombre.Text;
+            dtgvProductos.Rows[n].Cells[2].Value = txtPrecio.Text;
+
+            // Limpiar los textboxes
+            txtCodigo.Clear();
+            txtNombre.Clear();
+            txtPrecio.Clear();
+        }
+
+        private void dtgvProductos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            n = e.RowIndex;
+
+            if (n != -1)
+            {
+                lblInformacion.Text =(string)dtgvProductos.Rows[n].Cells[1].Value;
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (n != -1)
+            {
+                dtgvProductos.Rows.RemoveAt(n);
+            }
+        }
+    }
+}
