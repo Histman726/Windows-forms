f87aa47cb918b2c45e5d080ba03189f977bf8476
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-07-02 17:40:08 -0500

    Herramienta ColorDialog

---
+++ Windows Forms/Herramienta ColorDialog/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Herramienta_ColorDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lblMensaje.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            colorDialog1.Color = Color.FromArgb(12,20,52);
+        }
+    }
+}
